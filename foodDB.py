2f4b6929b95bc8ff795204a3ac56b315e08ac5fa
Rohini517 <77781975+Rohini517@users.noreply.github.com>
2021-06-19 17:14:04 +0530

    Add files via upload

---
+++ foodDB.py
@@ -0,0 +1,26 @@
+foodData =[{
+    "food_name" : "Briyani",
+    "food_img" : "https://res.cloudinary.com/swiggy/image/upload/f_auto,q_auto,fl_lossy/wmobv761tsifvaisa9lg",
+    "food_desc" : "Biryani is a mixed rice dish originating among the Muslims of the Indian subcontinent. It is made with Indian spices, rice, and meat, and sometimes, in addition, eggs and/or vegetables such as potatoes in certain regional varieties.",
+    "status"   : "Delivered",
+    "comments" : 32,
+},
+{
+    "food_name" : "Dosa",
+    "food_img" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIt5_8ifN-Qabr5pvBtIn6vHaAhvNFtmJgUg&usqp=CAU",
+    "food_desc" : "A dosa is a thin pancake or crepe, originating from South India, made from a fermented batter predominantly consisting of lentils and rice. It is somewhat similar to a crepe in appearance.",
+    "status"   : "Cooking",
+    "comments" : 40,
+},
+{
+    "food_name" : "Cake",
+    "food_img" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhXk_d0Ejhsg0hBWLroPgZjBy5qrpPBcXX3g&usqp=CAU",
+    "food_desc" : "Cake is a form of sweet food made from flour, sugar, and other ingredients, that is usually baked. In their oldest forms, cakes were modifications of bread, but cakes now cover a wide range of preparations .",
+    "status"   : "Ordered",
+    "comments" : 100,
+}
+]
+
+
+
+
---
+++ post/admin.py
@@ -0,0 +1,6 @@
+from post.models import PostTable
+from django.contrib import admin
+from post.models import PostTable
+
+# Register your models here.
+admin.site.register(PostTable)
